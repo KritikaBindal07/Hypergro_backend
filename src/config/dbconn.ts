647f62f038bc8c318c762fbea49ecc979cf8e021
kirtibindalkb770@gmail.com <kirtibindalkb770@gmail.com>
2025-05-31 11:32:14 +0530

    timeout fix

--- src/config/dbconn.ts
+++ src/config/dbconn.ts
@@ -2,8 +2,11 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = (await mongoose.connect(process.env.MONGODB_URI as string))
-      .connection;
+    const conn = (
+      await mongoose.connect(process.env.MONGODB_URI as string, {
+        connectTimeoutMS: 30000,
+      })
+    ).connection;
     console.log("database connected succesfully");
   } catch (error) {
     console.log("database error");
